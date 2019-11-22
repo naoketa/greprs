0127f73e585279ad22d37ca2d10b60ddc651567e
naoketa <49198895+naoketa@users.noreply.github.com>
2019-11-22 15:22:10 +0900

    とりあえず検索できようにする

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::io::Read;
 use std::env;
+use std::fs::File;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,6 +9,33 @@ fn main() {
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
+
+    run(config);
+}
+
+fn run(config: Config) {
+    let mut f = File::open(config.filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something wrong with reading the file");
+    
+    let results = search(&config.query, &contents);
+    
+    for line in results {
+        println!("{}", line);
+    }
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 struct Config {
